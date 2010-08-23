a8f8ce611d85c9b7719b65f53c1b8f3f78704632
volodymyr Hotsyk <gotsyk@gmail.com>
2010-08-23 19:28:34 +0300

    Fixed urls

--- odesk_auth/urls.py
+++ odesk_auth/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'pscore.auth.views.login'),
-    url(r'^callback/$', 'pscore.auth.views.callback'),
+    url(r'^login/$', 'odesk_auth.views.login'),
+    url(r'^callback/$', 'odesk_auth.views.callback'),
 )
